4814b084bce1d486e1edfeda6f6d326df93229b3
Akash-Maji <akashmaji556@gmail.com>
2024-01-22 10:10:54 +0530

    first commit

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,5 @@
-export const BASE_URL = 'http://localhost:4000/api/v1'
+//export const BASE_URL = 'http://localhost:4000/api/v1'
 //export const BASE_URL = 'https://indie-tour-travel-website-backend.onrender.com'
 // export const BASE_URL = 'http://mern-travel-booking-api.vercel.app/'
+
+export const BASE_URL = 'https://indie-tour-and-travel-website.vercel.app/'
